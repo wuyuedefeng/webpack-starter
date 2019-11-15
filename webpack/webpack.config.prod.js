60964382e1eebf7698e239c0b2ea40fb8dd0fd63
sanmu <sanmu@syedu.com>
2019-11-15 17:28:50 +0800

    FEAT: prod remove sourceMap

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -5,7 +5,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
+  // devtool: 'source-map',
+  devtool: false,
   stats: 'errors-only',
   bail: true,
   output: {
